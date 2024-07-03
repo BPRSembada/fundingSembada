4013da97ffbbb5be9acdd96ede6a3b9a45220684
Christopher Joshua <115674460+iWasKriss@users.noreply.github.com>
2024-07-03 15:42:06 +0700

    sw

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { InputGroup, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { InputGroup, Form, Button, Modal } from "react-bootstrap";
 import { MdOutlineAccountCircle, MdOutlineDateRange } from "react-icons/md";
 import { FiPhone } from "react-icons/fi";
 import { useForm } from "react-hook-form";
@@ -11,7 +11,13 @@ const Form_funding = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const handleClose = () => setShowModal(false);
 
   const onSubmit = async (data) => {
     try {
@@ -29,109 +35,130 @@ const Form_funding = () => {
 
       const result = await response.json();
       console.log(result);
-      alert(result.message);
+      setModalMessage(result.message);
+      setShowModal(true);
+      reset();
     } catch (error) {
       console.error(error);
-      alert("There was an error submitting the form");
+      setModalMessage("There was an error submitting the form");
+      setShowModal(true);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>Form pengajuan Deposito</h2>
-      <div className="formDepo">
-        <Form.Label className="form_label">Nama</Form.Label>
-        <InputGroup>
-          <InputGroup.Text id="basic-addon1">
-            <MdOutlineAccountCircle />
-          </InputGroup.Text>
-          <Form.Control
-            placeholder="Nama"
-            aria-label="Nama"
-            aria-describedby="basic-addon1"
-            {...register("nama", { required: "Nama is required" })}
-          />
-        </InputGroup>
-        <Form.Text className="mb-3">
-          {errors.nama && <span>{errors.nama.message}</span>}
-        </Form.Text>
-      </div>
-      <div className="formDepo">
-        <Form.Label className="form_label">No Handphone</Form.Label>
-        <InputGroup>
-          <InputGroup.Text id="basic-addon1">
-            <FiPhone />
-          </InputGroup.Text>
-          <Form.Control
-            type="number"
-            placeholder="08xxxxxxxxxx"
-            aria-label="No HP"
-            aria-describedby="basic-addon1"
-            {...register("no_hp", { required: "No HP is required" })}
-          />
-        </InputGroup>
-        <Form.Text className="mb-3">
-          {errors.no_hp && <span>{errors.no_hp.message}</span>}
-        </Form.Text>
-      </div>
-      <div className="formDepo">
-        <Form.Label className="form_label">Nominal</Form.Label>
-        <InputGroup>
-          <InputGroup.Text id="basic-addon1">Rp</InputGroup.Text>
-          <Form.Control
-            type="number"
-            placeholder="10.000.000"
-            aria-label="Nominal"
-            aria-describedby="basic-addon1"
-            {...register("nominal", { required: "Nominal is required" })}
-          />
-        </InputGroup>
-        <Form.Text className="mb-3">
-          {errors.nominal && <span>{errors.nominal.message}</span>}
-        </Form.Text>
-      </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Form pengajuan Deposito</h2>
+        <div className="formDepo">
+          <Form.Label className="form_label">Nama</Form.Label>
+          <InputGroup>
+            <InputGroup.Text id="basic-addon1">
+              <MdOutlineAccountCircle />
+            </InputGroup.Text>
+            <Form.Control
+              placeholder="Nama"
+              aria-label="Nama"
+              aria-describedby="basic-addon1"
+              {...register("nama", { required: "Isi nama anda" })}
+            />
+          </InputGroup>
+          <Form.Text className="mb-3">
+            {errors.nama && <span>{errors.nama.message}</span>}
+          </Form.Text>
+        </div>
+        <div className="formDepo">
+          <Form.Label className="form_label">No Handphone</Form.Label>
+          <InputGroup>
+            <InputGroup.Text id="basic-addon1">
+              <FiPhone />
+            </InputGroup.Text>
+            <Form.Control
+              type="number"
+              placeholder="08xxxxxxxxxx"
+              aria-label="No HP"
+              aria-describedby="basic-addon1"
+              {...register("no_hp", { required: "Isi nomor handphone anda" })}
+            />
+          </InputGroup>
+          <Form.Text className="mb-3">
+            {errors.no_hp && <span>{errors.no_hp.message}</span>}
+          </Form.Text>
+        </div>
+        <div className="formDepo">
+          <Form.Label className="form_label">Nominal</Form.Label>
+          <InputGroup>
+            <InputGroup.Text id="basic-addon1">Rp</InputGroup.Text>
+            <Form.Control
+              type="number"
+              placeholder="10.000.000"
+              aria-label="Nominal"
+              aria-describedby="basic-addon1"
+              {...register("nominal", {
+                required: "Isi jumlah penempatan yang akan dilakukan",
+              })}
+            />
+          </InputGroup>
+          <Form.Text className="mb-3">
+            {errors.nominal && <span>{errors.nominal.message}</span>}
+          </Form.Text>
+        </div>
 
-      <div className="formDepo">
-        <Form.Label className="form_label">Jangka Waktu</Form.Label>
-        <InputGroup>
-          <InputGroup.Text id="basic-addon1">
-            <MdOutlineDateRange />
-          </InputGroup.Text>
-          <Form.Control
-            placeholder="Jangka Waktu"
-            aria-label="Jangka Waktu"
-            aria-describedby="basic-addon1"
-            {...register("tenor", { required: "Tenor is required" })}
-          />
-        </InputGroup>
-        <Form.Text className="mb-3">
-          {errors.tenor && <span>{errors.tenor.message}</span>}
-        </Form.Text>
-      </div>
+        <div className="formDepo">
+          <Form.Label className="form_label">Jangka Waktu</Form.Label>
+          <InputGroup>
+            <InputGroup.Text id="basic-addon1">
+              <MdOutlineDateRange />
+            </InputGroup.Text>
+            <Form.Control
+              placeholder="Jangka Waktu"
+              aria-label="Jangka Waktu"
+              aria-describedby="basic-addon1"
+              {...register("tenor", { required: "Isi jangka waktu" })}
+            />
+          </InputGroup>
+          <Form.Text className="mb-3">
+            {errors.tenor && <span>{errors.tenor.message}</span>}
+          </Form.Text>
+        </div>
 
-      <div className="formDepo">
-        <Form.Label className="form_label">Suku Bunga</Form.Label>
-        <InputGroup>
-          <InputGroup.Text id="basic-addon1">%</InputGroup.Text>
-          <Form.Control
-            placeholder="Suku Bunga"
-            aria-label="Suku Bunga"
-            aria-describedby="basic-addon1"
-            {...register("bunga", { required: "Bunga is required" })}
-          />
-        </InputGroup>
-        <Form.Text className="mb-3">
-          {errors.bunga && <span>{errors.bunga.message}</span>}
-        </Form.Text>
-      </div>
+        <div className="formDepo">
+          <Form.Label className="form_label">Suku Bunga</Form.Label>
+          <InputGroup>
+            <InputGroup.Text id="basic-addon1">%</InputGroup.Text>
+            <Form.Control
+              placeholder="Suku Bunga"
+              aria-label="Suku Bunga"
+              aria-describedby="basic-addon1"
+              {...register("bunga", {
+                required: "Isi suku bunga yg anda inginkan",
+              })}
+            />
+          </InputGroup>
+          <Form.Text className="mb-3">
+            {errors.bunga && <span>{errors.bunga.message}</span>}
+          </Form.Text>
+        </div>
 
-      <div>
-        <Button variant="danger" className="me-4" type="submit">
-          Ajukan Sekarang
-        </Button>
-        <Button variant="danger">Kalkulator Deposito</Button>
-      </div>
-    </form>
+        <div>
+          <Button variant="danger" className="me-4" type="submit">
+            Ajukan Sekarang
+          </Button>
+          <Button variant="danger">Kalkulator Deposito</Button>
+        </div>
+      </form>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Data Tersimpan</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modalMessage}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
